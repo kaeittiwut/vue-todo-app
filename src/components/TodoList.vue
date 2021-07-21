667ce1315b07ecc77a169c82ece1ca5841b6d34e
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2021-07-22 00:16:37 +0700

    extract out a TodoItem component and use props and custom events to communicate between child and parent components

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,36 +12,15 @@
       enter-active-class="animated fadeInUp"
       leave-active-class="animated fadeOutDown"
     >
-      <div
+      <todo-item
         v-for="(todo, index) in todosFiltered"
         :key="todo.id"
-        class="todo-item"
-      >
-        <div class="todo-item-left">
-          <input type="checkbox" v-model="todo.completed" />
-          <div
-            v-if="!todo.editing"
-            @dblclick="editTodo(todo)"
-            class="todo-item-label"
-            :class="{ completed: todo.completed }"
-          >
-            {{ todo.title }}
-          </div>
-          <input
-            v-else
-            class="todo-item-edit"
-            type="text"
-            v-model="todo.title"
-            @blur="doneEdit(todo)"
-            @keyup.enter="doneEdit(todo)"
-            @keyup.esc="cancelEdit(todo)"
-            v-focus
-          />
-        </div>
-        <div class="remove-item" @click="removeTodo(index)">
-          &times;
-        </div>
-      </div>
+        :todo="todo"
+        :index="index"
+        :checkAll="!anyRemaining"
+        @removeTodo="removeTodo"
+        @finishedEdit="finishedEdit"
+      ></todo-item>
     </transition-group>
 
     <div class="extra-container">
@@ -89,8 +68,13 @@
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
+
 export default {
   name: "todo-list",
+  components: {
+    TodoItem
+  },
   data() {
     return {
       newTodo: "",
@@ -134,13 +118,6 @@ export default {
       return this.todos.filter(todo => todo.completed).length > 0;
     }
   },
-  directives: {
-    focus: {
-      inserted: function(el) {
-        el.focus();
-      }
-    }
-  },
   methods: {
     addTodo() {
       if (this.newTodo.trim().length == 0) {
@@ -155,20 +132,6 @@ export default {
       this.newTodo = "";
       this.idForTodo++;
     },
-    editTodo(todo) {
-      this.beforeEditCache = todo.title;
-      todo.editing = true;
-    },
-    doneEdit(todo) {
-      if (todo.title.trim() == "") {
-        todo.title = this.beforeEditCache;
-      }
-      todo.editing = false;
-    },
-    cancelEdit(todo) {
-      todo.title = this.beforeEditCache;
-      todo.editing = false;
-    },
     removeTodo(index) {
       this.todos.splice(index, 1);
     },
@@ -177,6 +140,9 @@ export default {
     },
     clearCompleted() {
       this.todos = this.todos.filter(todo => !todo.completed);
+    },
+    finishedEdit(data) {
+      this.todos.splice(data.index, 1, data.todo);
     }
   }
 };
